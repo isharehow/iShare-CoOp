6b708e7af99e829959f15bfedea26bb0b6a1ea53
Jamel Brown <jamel@ishare.solutions>
2019-01-09 19:48:17 -0500

    Add files via upload

---
+++ Python_Lessons_Cheat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def simple_addition (num1,num2):
+    list = [3, 5, 2, 7]
+    answer = num1 + num2
+    print('num1 is', num1)
+    print('num2 is', num2)
+    print('answer is',answer)
+    print('answer doubled is',answer*2)
+
+simple_addition(7,12)
---
+++ simple-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def simple_addition (num1,num2):
+    answer= num1 + num2
+    print('num1 is', num1)
+    print('num2 is', num2)
+    print('anmswer is',answer)
+
+simple_addition(7,12)
+
